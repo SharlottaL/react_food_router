94a25e4fa5869792bab5dbb389d7c720fd1bc4ce
SharlottaL <botanichkagigantika@gmail.com>
2026-03-11 15:34:28 +0300

    Categories DONE, Recipe in progress

---
+++ src/api.js
@@ -0,0 +1,18 @@
+import { API_URL } from "./config";
+
+const getAllCategories = async () =>
+{
+    const response = await fetch(API_URL + '/categories.php');
+    return await response.json();
+}
+const getMealById = async (idMeal) =>
+{
+    const response = await fetch(API_URL + '/lookup.php?i=' + idMeal);
+    return await response.json();
+}
+const getFilteredCategory = async (carName) =>
+{
+    const response = await fetch(API_URL + '/filter.php?c=' + carName);
+    return await response.json();
+}
+export { getMealById, getAllCategories, getFilteredCategory }
---
+++ src/components/MealList.js
@@ -0,0 +1,20 @@
+import './MealList.css';
+import Meal from './Meal';
+
+function MealList({meals})
+{
+    return(
+<div className='list'>
+    {
+        meals.map
+        (
+            meal =>
+            (
+                <Meal key={meal.idMeal} {...meal} />
+            )
+        )
+    }
+</div>
+    )
+}
+export default MealList;
